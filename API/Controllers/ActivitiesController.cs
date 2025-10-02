3e32281c0814d39ea7e86f795954932f2c0c6925
Igor <igortadic.msc@gmail.com>
2025-10-02 22:29:38 +0200

    making the API Controllers thinner

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,21 +1,20 @@
 using Application.Activities.Queries;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await mediator.Send(new GetActivityDetails.Query { Id = id });
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
