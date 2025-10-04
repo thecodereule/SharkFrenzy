a073cd5d1079b4032203b65122a0fb3b28608a41
Igor <igortadic.msc@gmail.com>
2025-10-04 18:19:31 +0200

    Creating a card for the activities

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
-import { Container, CssBaseline } from "@mui/material"
+import { Box, Container, CssBaseline } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Navbar from "./NavBar"
-import ActivityDashboard from "../../features/activities/ActivityDashboard"
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard"
 
 
 function App() {
@@ -17,13 +17,13 @@ function App() {
 
 
   return (
-    <>
+    <Box sx={{bgcolor: '#eeeeee'}}>
       <CssBaseline />
       <Navbar />
       <Container maxWidth='xl' sx={{mt: 3}}>
         <ActivityDashboard activities={activities} />
       </Container>
-    </>
+    </Box>
   )
 }
 
