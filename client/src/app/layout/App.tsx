e95098efb86f2bb258a304b0303b66d0e4367841
Igor <igortadic.msc@gmail.com>
2025-10-07 23:53:17 +0200

    Adding a home page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,19 +1,25 @@
 import { Box, Container, CssBaseline } from "@mui/material"
 import Navbar from "./NavBar"
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
+import HomePage from "../../features/home/HomePage";
 
 
 function App() {
-
+  const location = useLocation();
   return (
     <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
       <CssBaseline />
-      <Navbar />
-      <Container maxWidth='xl' sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <Navbar />
+          <Container maxWidth='xl' sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   )
 }
 
 export default App
+ 
