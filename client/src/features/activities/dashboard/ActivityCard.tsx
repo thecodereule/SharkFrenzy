b6476d5a3ff18f77ca2492d768e3ba1b7f707e7b
Igor <igortadic.msc@gmail.com>
2025-10-07 18:37:31 +0200

    Styling the Activity Card

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
-import { useActivities } from "../../../lib/hooks/useActivities";
+import AccessTime from "@mui/icons-material/AccessTime";
+import Place from "@mui/icons-material/Place";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, Chip, Divider, Typography } from "@mui/material";
 import { Link } from "react-router";
 
 type Props = {
@@ -7,22 +8,62 @@ type Props = {
 }
 
 export default function ActivityCard({ activity }: Props) {
-    const { deleteActivity } = useActivities();
+    const isHost = false;
+    const isGoing = false;
+    const label = isHost ? 'You are hosting this activity' : isGoing ? 'You are going to this activity' : '';
+    const isCancelled = false;
+    const color = isHost ? 'secondary' : isGoing ? 'warning' : 'default';
+
     return (
-        <Card sx={{ borderRadius: 3 }}>
-            <CardContent>
-                <Typography variant="h5">{activity.title}</Typography>
-                <Typography sx={{ color: 'text.secondary', mb: 1 }}>{activity.date}</Typography>
-                <Typography variant="body2">{activity.description}</Typography>
-                <Typography variant="subtitle1">{activity.city} / {activity.venue}</Typography>
-            </CardContent>
-            <CardActions sx={{ display: 'flex', justifyContent: 'space-between', pb: 2 }}>
-                <Chip label={activity.category} variant='outlined' />
-                <Box display='flex' gap={3}>
-                    <Button component={Link} to={`/activities/${activity.id}`} size="medium" variant="contained">View</Button>
-                    <Button onClick={() => deleteActivity.mutate(activity.id)} disabled={deleteActivity.isPending} color='error' size="medium" variant="contained">Delete</Button>
+        <Card elevation={3} sx={{ borderRadius: 3 }}>
+            <Box display='flex' alignItems='center' justifyContent='space-between'>
+                <CardHeader
+                    avatar={<Avatar sx={{ height: 80, width: 80 }} />}
+                    title={activity.title}
+                    typographyProps={{
+                        fontWeight: 'bold',
+                        fontSize: 20
+                    }}
+                    subheader={
+                        <>
+                            Hosted by {' '} <Link to={`/profiles/bob`}>Bob </Link>
+                        </>
+                    }
+                />
+                <Box display='flex' flexDirection='column' gap={2} mr={2}>
+                    {(isHost || isGoing) && <Chip label={label} color={color} sx={{ borderRadius: 2 }}></Chip>}
+                    {isCancelled && <Chip label='Cancelled' color='error' sx={{ borderRadius: 2 }}></Chip>}
                 </Box>
-            </CardActions>
+            </Box>
+
+            <Divider sx={{ mb: 3 }} />
+
+
+            <CardContent sx={{ p: 0 }}>
+                <Box display='flex' alignItems={'center'} mb={2} px={2}>
+                    <AccessTime sx={{ mr: 1 }} />
+                    <Typography variant="body2">{activity.date}</Typography>
+                    <Place sx={{ mr: 1, ml: 3 }} />
+                    <Typography variant='body2'>{activity.venue}</Typography>
+                </Box>
+                <Divider />
+                <Box display='flex' gap={2} sx={{ backgroundColor: 'grey.200', py: 3, pl: 3 }}>
+                    Attendees go here
+                </Box>
+            </CardContent>
+            <CardContent sx={{ pb: 2 }}>
+                <Typography variant='body2'>
+                    {activity.description}
+                </Typography>
+                <Button
+                    component={Link}
+                    to={`/activities/${activity.id}`}
+                    size="medium" variant="contained"
+                    sx={{ display: 'flex', justifySelf: 'self-end', borderRadius: 3 }}
+                >
+                    View
+                </Button>
+            </CardContent>
         </Card>
     )
 }
