f8ef4ffcffc33ac0151512599fcd1d8047de2df3
Igor <igortadic.msc@gmail.com>
2025-10-08 23:37:34 +0200

    Computed properties

--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -4,6 +4,9 @@ import {makeAutoObservable} from 'mobx';
 export default class CounterStore {
     title = 'Counter store';
     count = 42;
+    events: string[] = [
+        `Initial count is ${this.count}`
+    ]
 
     constructor() {
         makeAutoObservable(this)
@@ -11,9 +14,15 @@ export default class CounterStore {
 
     increment = (amount = 1) => {
         this.count  += amount;
+        this.events.push(`Incremented by ${amount} - count is now ${this.count}`)
     }
 
     decrement = (amount = 1) => {
         this.count -= amount;
+        this.events.push(`Decremented by ${amount} - count is now ${this.count}`)
+    }
+
+    get eventCount() {
+        return this.events.length;
     }
 }
