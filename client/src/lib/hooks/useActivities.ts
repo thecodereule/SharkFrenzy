ea3e9643265df49313a7cb15bcdd3c048ae13a76
Igor <igortadic.msc@gmail.com>
2025-10-08 23:51:50 +0200

    Adding loading state

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,8 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useLocation } from "react-router";
 
 export const useActivities = (id?: string) => {
     const queryClient = useQueryClient();
+    const location = useLocation();
 
     const { data: activities, isPending } = useQuery({
         queryKey: ['activities'],
@@ -10,7 +12,8 @@ export const useActivities = (id?: string) => {
             const response = await agent.get<Activity[]>('/activities');
             console.log('API Response:', response.data);
             return response.data;
-        }
+        },
+        enabled: !id && location.pathname === '/activities'
     });
 
     const {data: activity, isLoading: isLoadingActivity} = useQuery({
