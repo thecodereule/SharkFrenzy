b4224eaae22d1e5a7f53aa8f030de1842096cdb5
Igor <igortadic.msc@gmail.com>
2025-10-10 00:12:55 +0200

    Validating with Fluent Validation

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using Application.Activities.Queries;
+using Application.Activities.Validators;
 using Application.Core;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -16,7 +18,8 @@ builder.Services.AddCors();
 builder.Services.AddMediatR(x =>
     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Application/Activities/Validators/CreateActivityValidator.cs
@@ -0,0 +1,14 @@
+using System;
+using Application.Activities.Queries;
+using FluentValidation;
+
+namespace Application.Activities.Validators;
+
+public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
+{
+    public CreateActivityValidator()
+    {
+        RuleFor(x => x.ActivityDto.Title).NotEmpty().WithMessage("Title is required");
+        RuleFor(x => x.ActivityDto.Description).NotEmpty().WithMessage("Description is required");
+    }
+}
