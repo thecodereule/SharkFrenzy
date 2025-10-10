767709ddd54a346286b08e27909849f0152c95c3
Igor <igortadic.msc@gmail.com>
2025-10-10 02:10:28 +0200

    CreateActivity validation

--- Application/Activities/Validators/CreateActivityValidator.cs
+++ Application/Activities/Validators/CreateActivityValidator.cs
@@ -8,7 +8,24 @@ public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
 {
     public CreateActivityValidator()
     {
-        RuleFor(x => x.ActivityDto.Title).NotEmpty().WithMessage("Title is required");
-        RuleFor(x => x.ActivityDto.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.ActivityDto.Title)
+            .NotEmpty().WithMessage("Title is required")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters");
+        RuleFor(x => x.ActivityDto.Description)
+            .NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.ActivityDto.Date)
+            .GreaterThan(DateTime.UtcNow).WithMessage("Date must be in the future");
+        RuleFor(x => x.ActivityDto.Category)
+            .NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ActivityDto.City)
+            .NotEmpty().WithMessage("City is required");
+        RuleFor(x => x.ActivityDto.Venue)
+            .NotEmpty().WithMessage("Venue is required");
+        RuleFor(x => x.ActivityDto.Latitude)
+            .NotEmpty().WithMessage("Latitude is required")
+            .InclusiveBetween(-90, 90).WithMessage("Latitude must be between -90 and 90");
+        RuleFor(x => x.ActivityDto.Longitude)
+            .NotEmpty().WithMessage("Longitude is required")
+            .InclusiveBetween(-180, 180).WithMessage("Longitute needs to be between -180 and 180");
     }
 }
