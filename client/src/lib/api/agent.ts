addb104ebda298feda2dd1c4df960eb0249891dc
Igor <igortadic.msc@gmail.com>
2025-10-11 14:45:03 +0200

    Handling Bad Requests

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -28,10 +28,20 @@ agent.interceptors.response.use(
         await sleep(1000);
         store.uiStore.isIdle();
 
-        const { status } = error.response;
+        const { status, data } = error.response;
         switch(status) {
             case 400:
-                toast.error('Bad request')
+                if (data.errors) {
+                    const modalStateErrors = [];
+                    for (const key in data.errors) {
+                        if (data.errors[key]) {
+                            modalStateErrors.push(data.errors[key]);
+                        }
+                    }
+                    throw modalStateErrors.flat();
+                } else {
+                    toast.error(data);
+                }
                 break;
             case 401:
                 toast.error('Unauthorized')
