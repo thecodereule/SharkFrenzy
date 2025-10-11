eca4d117e4ef97f95338a74318a81ae2626820d0
Igor <igortadic.msc@gmail.com>
2025-10-11 16:03:44 +0200

    Creating a reusable Text Input

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,18 +1,19 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useParams } from "react-router";
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
-import {zodResolver} from '@hookform/resolvers/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import TextInput from "../../../app/shared/components/TextInput";
 
 export default function ActivityForm() {
-    const {register, reset, handleSubmit, formState: { errors }} = useForm<ActivitySchema>({
+    const { control, reset, handleSubmit } = useForm<ActivitySchema>({
         mode: 'onTouched',
         resolver: zodResolver(activitySchema)
     });
-    const {id} = useParams();
-    const { updateActivity, createActivity,  activity, isLoadingActivity } = useActivities(id);
+    const { id } = useParams();
+    const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
 
     useEffect(() => {
         if (activity) {
@@ -33,22 +34,22 @@ export default function ActivityForm() {
                 {activity ? 'Edit activity' : 'Create activity'}
             </Typography>
             <Box component="form" onSubmit={handleSubmit(onSubmit)} display='flex' flexDirection='column' gap={3}>
-                <TextField {...register('title')} label='Title' defaultValue={activity?.title} error={!!errors.title} helperText={errors.title?.message} />
-                <TextField {...register('description')} label='Description' defaultValue={activity?.description} multiline rows={3} error={!!errors.description} helperText={errors.description?.message} />
-                <TextField {...register('category')} label='Category' defaultValue={activity?.category} error={!!errors.category} helperText={errors.category?.message} />
-                <TextField {...register('date')} label='Date' type="date" 
-                    defaultValue={activity?.date 
-                    ? new Date(activity.date).toISOString().split('T')[0] 
-                    : new Date().toISOString().split('T')[0]
-                    } error={!!errors.date} helperText={errors.date?.message} InputLabelProps={{ shrink: true }} 
+                <TextInput label='Title' control={control} name='title' />
+                <TextInput label='Description' control={control} name='description' multiline rows={3} />
+                <TextInput label='Category' control={control} name='category' />
+                <TextInput label='Date' control={control} name='date' type="date"
+                    defaultValue={activity?.date
+                        ? new Date(activity.date).toISOString().split('T')[0]
+                        : new Date().toISOString().split('T')[0]
+                    }
                 />
-                <TextField {...register('city')} label='City' defaultValue={activity?.city} error={!!errors.city} helperText={errors.city?.message} />
-                <TextField {...register('venue')} label='Venue' defaultValue={activity?.venue} error={!!errors.venue} helperText={errors.venue?.message} />
+                <TextInput label='City' control={control} name='city' />
+                <TextInput label='Venue' control={control} name='venue' />
                 <Box display='flex' justifyContent='end' gap={3}>
                     <Button color='inherit'>Cancel</Button>
-                    <Button 
-                        type="submit" 
-                        variant='contained' 
+                    <Button
+                        type="submit"
+                        variant='contained'
                         color='success'
                         disabled={updateActivity.isPending || createActivity.isPending}
                     >Submit</Button>
