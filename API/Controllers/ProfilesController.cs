53470393c12b3d8c7775ed8e01fca0cd3b503d77
Igor <igortadic.msc@gmail.com>
2025-10-22 00:05:45 +0200

    Setting the main photo

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -26,4 +26,10 @@ public class ProfilesController : BaseApiController
     {
         return HandleResult(await Mediator.Send(new DeletePhoto.Command { PhotoId = photoId }));
     }
+
+    [HttpPut("{photoId}/setMain")]
+    public async Task<ActionResult> SetMainPhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
+    }
 }
