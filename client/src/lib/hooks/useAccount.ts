be2d9968eb341289e2f3b8442d4ea5de8fb08cfd
Igor <igortadic.msc@gmail.com>
2025-10-22 00:11:25 +0200

    solving user not being redirected to the homepage after login

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,14 +1,13 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import type { LoginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent"
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import type { RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
-    const location = useLocation();
 
     const loginUser = useMutation({
         mutationFn: async (creds: LoginSchema) => {
@@ -49,8 +48,6 @@ export const useAccount = () => {
             return response.data;
         },
         enabled: !queryClient.getQueryData(['user']) 
-        && location.pathname !== '/login'
-        && location.pathname !== '/register'
     })
 
     return {
