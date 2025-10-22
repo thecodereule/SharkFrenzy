fd5dd03c6bbdfea1d7b3786e851c538555884d49
Igor <igortadic.msc@gmail.com>
2025-10-22 12:47:29 +0200

    Adding React Dropzone

--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,10 +1,32 @@
-import { Grid2, Typography } from "@mui/material";
+import { CloudUpload } from "@mui/icons-material";
+import { Box, Grid2, Typography } from "@mui/material";
+import { useCallback } from "react";
+import { useDropzone } from 'react-dropzone'
+
 
 export default function PhotoUploadWidget() {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        console.log(acceptedFiles)
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={4}>
                 <Typography variant="overline" color="secondary">Step 1 - Add Photo</Typography>
+                <Box {...getRootProps()}
+                    sx={{
+                        border: 'dashed 3px #eee',
+                        borderColor: isDragActive ? 'green' : '#eee',
+                        borderRadius: '5px',
+                        paddingTop: '30px',
+                        textAlign: 'center',
+                        height: '280px'
+                    }}
+                >
+                    <input {...getInputProps()} />
+                    <CloudUpload sx={{fontSize: 80}} />
+                    <Typography variant="h5">Drop image here</Typography>
+                </Box>
             </Grid2>
             <Grid2 size={4}>
                 <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
