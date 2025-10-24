52a54c1443f22a828221b0fb1638821df818578f
Igor <igortadic.msc@gmail.com>
2025-10-25 00:18:45 +0200

    Configuring the chat component

--- client/src/features/activities/details/ActivityDetailsChat.tsx
+++ client/src/features/activities/details/ActivityDetailsChat.tsx
@@ -1,10 +1,11 @@
 import { Box, Typography, Card, CardContent, TextField, Avatar } from "@mui/material";
 import { Link, useParams } from "react-router";
 import { useComments } from "../../../lib/hooks/useComments";
+import { timeAgo } from "../../../lib/util/util";
 
 export default function ActivityDetailsChat() {
-    const {id} = useParams();
-    const {commentStore} = useComments(id);
+    const { id } = useParams();
+    const { commentStore } = useComments(id);
 
     return (
         <>
@@ -32,22 +33,24 @@ export default function ActivityDetailsChat() {
                         </form>
                     </div>
 
-                    <Box>
-                        <Box sx={{ display: 'flex', my: 2 }}>
-                            <Avatar src={'/images/user.png'} alt={'user image'} sx={{ mr: 2 }} />
-                            <Box display='flex' flexDirection='column'>
-                                <Box display='flex' alignItems='center' gap={3}>
-                                    <Typography component={Link} to={`/profiles/username`} variant="subtitle1" sx={{ fontWeight: 'bold', textDecoration: 'none' }}>
-                                        Bob
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        2 hours ago
-                                    </Typography>
-                                </Box>
+                    <Box sx={{ height: 400, overflow: 'auto' }}>
+                        {commentStore.comments.map(comment => (
+                            <Box key={comment.id} sx={{ display: 'flex', my: 2 }}>
+                                <Avatar src={comment.imageUrl} alt={'user image'} sx={{ mr: 2 }} />
+                                <Box display='flex' flexDirection='column'>
+                                    <Box display='flex' alignItems='center' gap={3}>
+                                        <Typography component={Link} to={`/profiles/${comment.userId}`} variant="subtitle1" sx={{ fontWeight: 'bold', textDecoration: 'none' }}>
+                                            {comment.displayName}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            {timeAgo(comment.createdAt)}
+                                        </Typography>
+                                    </Box>
 
-                                <Typography sx={{ whiteSpace: 'pre-wrap' }}>Comment goes here</Typography>
+                                    <Typography sx={{ whiteSpace: 'pre-wrap' }}>{comment.body}</Typography>
+                                </Box>
                             </Box>
-                        </Box>
+                        ))}
                     </Box>
                 </CardContent>
             </Card>
