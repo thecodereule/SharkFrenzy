b0897e52b7883ef9931c64e7885f4a5ed79dfeb1
Igor <igortadic.msc@gmail.com>
2025-10-24 23:16:52 +0200

    Creating a list handler for the comments

--- Domain/Comment.cs
+++ Domain/Comment.cs
@@ -10,7 +10,7 @@ public class Comment
 
     // Nav properties
     public required string UserId { get; set; }
-    public required User User { get; set; }
+    public User User { get; set; } = null!;
     public required string ActivityId { get; set; }
-    public required Activity Activity { get; set; }
+    public Activity Activity { get; set; } = null!;
 }
