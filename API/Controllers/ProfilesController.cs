5dbdfd6350b8aa4a70b509aac0482f340282424a
Igor <igortadic.msc@gmail.com>
2025-10-26 13:02:14 +0100

    Creating a follow toggle handle

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -40,4 +40,10 @@ public class ProfilesController : BaseApiController
     {
         return HandleResult(await Mediator.Send(new GetProfile.Query { UserId = userId }));
     }
+
+    [HttpPost("{userId}/follow")]
+    public async Task<ActionResult> FollowToggle(string userId)
+    {
+        return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUserId = userId }));
+    }
 }
