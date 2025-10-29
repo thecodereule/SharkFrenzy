c406fd33a239ae019586353b5140327bdbfd6296
Igor <igortadic.msc@gmail.com>
2025-10-29 19:47:35 +0100

    Adding infinite scrolling

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,13 @@
-import { Button, Grid2 } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityFilters from "./ActivityFilters";
-import { useActivities } from "../../../lib/hooks/useActivities";
 
 
 export default function ActivityDashboard() {
-    const { isFetchingNextPage, fetchNextPage, hasNextPage } = useActivities()
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={8}>
                 <ActivityList />
-                <Button
-                    onClick={() => fetchNextPage()}
-                    sx={{ my: 2, float: 'right' }}
-                    variant="contained"
-                    disabled={!hasNextPage || isFetchingNextPage}
-                >
-                    Load more
-                </Button>
             </Grid2>
             <Grid2
                 size={4}
--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,11 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { Fragment } from "react/jsx-runtime";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 
 export default function ActivityList() {
-    const { activitiesGroup, isLoading } = useActivities();
+    const { activitiesGroup, isLoading, hasNextPage, fetchNextPage } = useActivities();
+    const {ref, inView} = useInView({
+        threshold: 0.5
+    });
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, fetchNextPage])
 
     if (isLoading) return <Typography>Loading...</Typography>
 
@@ -14,14 +24,20 @@ export default function ActivityList() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
             {activitiesGroup.pages.map((activities, index) => (
-                <Fragment key={index}>
+                <Box 
+                key={index}
+                ref={index === activitiesGroup.pages.length - 1 ? ref : null}
+                display='flex'
+                flexDirection='column'
+                gap={3}
+                >
                     {activities.items.map(activity => (
                         <ActivityCard
                             key={activity.id}
                             activity={activity}
                         />
                     ))}
-                </Fragment>
+                </Box>
             ))}
         </Box>
     )
