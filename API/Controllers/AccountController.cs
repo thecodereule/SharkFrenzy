d9fd86f053955ff90bc78bddb44f78831fe2e570
Igor <igortadic.msc@gmail.com>
2025-11-10 16:52:05 +0100

    Creating a resend account email service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Text;
 using API.DTOs;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(SignInManager<User> signInManager) : BaseApiController
+public class AccountController(SignInManager<User> signInManager, IEmailSender<User> emailSender, IConfiguration config) : BaseApiController
 {
     [AllowAnonymous]
     [HttpPost("register")]
@@ -22,7 +25,12 @@ public class AccountController(SignInManager<User> signInManager) : BaseApiContr
 
         var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
 
-        if (result.Succeeded) return Ok();
+        if (result.Succeeded)
+        {
+            await SendConfirmationEmailAsync(user, registerDto.Email);
+
+            return Ok();
+        }
 
         foreach (var error in result.Errors)
         {
@@ -30,6 +38,29 @@ public class AccountController(SignInManager<User> signInManager) : BaseApiContr
         }
 
         return ValidationProblem();
+    }
+
+    [AllowAnonymous]
+    [HttpGet("resendConfirmEmail")]
+    public async Task<ActionResult> ResendConfirmEmail(string email)
+    {
+        var user = await signInManager.UserManager.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+        if (user == null) return BadRequest("Invalid email");
+
+        await SendConfirmationEmailAsync(user, email);
+
+        return Ok();
+    }
+
+    private async Task SendConfirmationEmailAsync(User user, string email)
+    {
+        var code = await signInManager.UserManager.GenerateEmailConfirmationTokenAsync(user);
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+        var confirmEmailUrl = $"{config["ClientAppUrl"]}/confirm-email?userId={user.Id}&code={code}";
+
+        await emailSender.SendConfirmationLinkAsync(user, email, confirmEmailUrl);
     }
 
     [AllowAnonymous]
